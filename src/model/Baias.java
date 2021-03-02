f20995b6a44d7efac37c83d8dad89d9937abd12b
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2021-03-01 21:48:31 -0300

    create baia and funcionario class

---
+++ src/model/Baias.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Baias {
+	private String nomeBaia;
+	private String capacidade;
+	private String definicao;
+	private String funcionarioResponsavel;
+	
+	public String getNomeBaia() {
+		return nomeBaia;
+	}
+	public void setNomeBaia(String nomeBaia) {
+		this.nomeBaia = nomeBaia;
+	}
+	public String getCapacidade() {
+		return capacidade;
+	}
+	public void setCapacidade(String capacidade) {
+		this.capacidade = capacidade;
+	}
+	public String getDefinicao() {
+		return definicao;
+	}
+	public void setDefinicao(String definicao) {
+		this.definicao = definicao;
+	}
+	public String getFuncionarioResponsavel() {
+		return funcionarioResponsavel;
+	}
+	public void setFuncionarioResponsavel(String funcionarioResponsavel) {
+		this.funcionarioResponsavel = funcionarioResponsavel;
+	}
+}
---
+++ src/model/Funcionario.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Funcionario {
+	private String nome;
+	private String funcao;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getFuncao() {
+		return funcao;
+	}
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+}
